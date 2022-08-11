fb59ab691eb51ab3a0c68e6a7976ca6723e2fe16
AlefMemTav <190115548@aluno.unb.br>
2022-08-11 20:08:45 -0300

    Concluida a tabela

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Tabela de livros</h1>
-    </div>
-  );
+import TableHead from './components/TableHead';
+import TableFoot from './components/TableFoot';
+import TableBody from './components/TableBody';
+
+class App extends Component {
+  state = {
+    books: []
+  };
+
+  componentDidMount(){
+    fetch("/api/livros.json")
+    .then(response => response.json())
+    .then(books => this.setState({books}))
+    .catch(function(error){
+      console.log("Erro na requisição");
+    })
+    .finally(function(){
+      console.log("Sempre retorna");
+    });
+  }
+
+  handleRemoveRow = (id) => {
+    const books = this.state.books.filter(l => l.id !== id);
+    this.setState({books});
+    console.log("Botão clicado");
+  }
+
+  handleOrdenarCrescente = (title) =>{
+    const books = this.state.books.sort((a, b) =>
+    a.title < b.title ? -1 : 0
+    );
+    this.setState({books});
+  };
+
+  handleOrdenarDecrescente = (title) =>{
+    const books = this.state.books.sort((a, b) =>
+    a.title < b.title ? -1 : 0
+    );
+    books.reverse();
+    this.setState({books});
+  };
+
+
+  render(){
+    return (
+      <table className="table">
+        <TableHead
+          ordenarCrescente={this.handleOrdenarCrescente}
+          ordenarDecrescente={this.handleOrdenarDecrescente}
+         />
+        <TableBody 
+          books = {this.state.books}
+          removeRow = {this.handleRemoveRow}
+        />
+        <TableFoot nmbBooks = {this.state.books.length} />
+      </table>
+    );
+  }
 }
 
 export default App;
