f1a390325746e1d57fe8741ef4d24ed642e07839
AlefMemTav <190115548@aluno.unb.br>
2022-10-26 22:22:46 -0300

    Props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,22 @@
 import React from "react";
 
-const Home = () => {
+const Home = props => {
+  console.log(props);
   return (
     <>
-      <h2>Últimos lançamentos</h2>
-      <div className="card">
-        <div className="thumb">
-          <img src="imagens/capas/9788575228142.jpg" alt="" />
-        </div>
-        <div className="detalhes">
-          <h3>Padrões para Kubernetes</h3>
-          <p>O modo como os ...</p>
-          <a href="#">Leia mais &gt;</a>
-        </div>
-      </div>
-      <div className="card">
-        <div className="thumb">
-          <img src="imagens/capas/9788575228074.jpg" alt="" />
-        </div>
-        <div className="detalhes">
-          <h3>Introdução ao Pentest – 2ª Edição</h3>
-          <p>Introdução ao Pentest...</p>
-          <a href="#">Leia mais &gt;</a>
-        </div>
-      </div>
+      <h3>{props.tituloPagina}</h3>
+      {/* <p>
+        ISBN: {props.isbn}<br />
+        Titulo: {props.titulo}<br />
+        Autor: {props.autor}
+      </p> */}
+      {props.dados.map((item,i) => (
+        <p>
+          ISBN: {props.dados[i].isbn}<br />
+          Titulo: {props.dados[i].titulo}<br />
+          Autor: {props.dados[i].autor}
+        </p>
+      ))}
     </>
   );
 };
