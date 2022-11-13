ee5ae98fce02a79552e2902877d4e0950d4e70e5
AlefMemTav <190115548@aluno.unb.br>
2022-11-13 15:42:56 -0300

    API de Livros

--- src/App.js
+++ src/App.js
@@ -5,28 +5,49 @@ import Topo from "./components/Topo";
 import Home from "./components/Home";
 import Catalogo from "./components/Catalogo";
 import NotFound from "./components/NotFound";
-import Principal from "./components/Principal";
+//import Principal from "./components/Principal";
 import Rodape from "./components/Rodape";
 import Programacao from "./components/Programacao";
 import Design from "./components/Design";
 import Frontend from "./components/Frontend";
 import "./index.css";
+import Livro from "./components/Livro";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    livros: []
+  };
+
+  async componentDidMount(){
+    try{
+      const {data: livros} = await axios.get("/api/todosOsLivros.json");
+      this.setState({livros});
+    }catch(error){
+      console.log(error);
+      document
+      .querySelectorAll(".principal")[0]
+      .insertAdjacentHTML(
+        "beforeend",
+        "<p class='erro'>Mensagem de erro</p>"
+      );
+    }
+  }
+
   render() {
     return (
     <Router>
       <>
         <Topo/>
           <Routes>
-            <Route exact path="/" render={Home} />
-            <Route exact path="/frontend" render={() => <Frontend />} />
-            <Route exact path="/programacao" render={() => <Programacao />} />
-            <Route exact path="/design" render={() => <Design />} />
-            <Route exact path="/catalogo" render={(props) => <Catalogo />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/frontend" element={<Frontend />} />
+            <Route exact path="/programacao" element={<Programacao />} />
+            <Route exact path="/design" element={<Design />} />
+            <Route exact path="/catalogo" element={<Catalogo />} />
             <Route component={NotFound} />
           </Routes>
-          <Rodape />
+        <Rodape />
       </>
     </Router>
     );
---
+++ src/components/Livro.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Livro = () => {
+  return (
+    <div>Livro</div>
+  )
+}
+
+export default Livro
