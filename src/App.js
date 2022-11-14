090bb215999acbe87d4b1fb3e83972b8d1b95b64
AlefMemTav <190115548@aluno.unb.br>
2022-11-14 15:18:53 -0300

    Criado tabela e menu

--- src/App.js
+++ src/App.js
@@ -1,61 +1,14 @@
-import React, {Component} from "react";
-
-import TableHead from './components/TableHead';
-import TableFoot from './components/TableFoot';
-import TableBody from './components/TableBody';
+import React, { Component } from "react";
+import Menu from "./components/Menu";
+import TabelaLivros from "./components/TabelaLivros";
 
 class App extends Component {
-  state = {
-    books: []
-  };
-
-  componentDidMount(){
-    fetch("/api/livros.json")
-    .then(response => response.json())
-    .then(books => this.setState({books}))
-    .catch(function(error){
-      console.log("Erro na requisição");
-    })
-    .finally(function(){
-      console.log("Sempre retorna");
-    });
-  }
-
-  handleRemoveRow = (id) => {
-    const books = this.state.books.filter(l => l.id !== id);
-    this.setState({books});
-    console.log("Botão clicado");
-  }
-
-  handleOrdenarCrescente = (title) =>{
-    const books = this.state.books.sort((a, b) =>
-    a.title < b.title ? -1 : 0
-    );
-    this.setState({books});
-  };
-
-  handleOrdenarDecrescente = (title) =>{
-    const books = this.state.books.sort((a, b) =>
-    a.title < b.title ? -1 : 0
-    );
-    books.reverse();
-    this.setState({books});
-  };
-
-
-  render(){
+  render() {
     return (
-      <table className="table">
-        <TableHead
-          ordenarCrescente={this.handleOrdenarCrescente}
-          ordenarDecrescente={this.handleOrdenarDecrescente}
-         />
-        <TableBody 
-          books = {this.state.books}
-          removeRow = {this.handleRemoveRow}
-        />
-        <TableFoot nmbBooks = {this.state.books.length} />
-      </table>
+      <div className="App">
+        <Menu />
+        <TabelaLivros />
+      </div>
     );
   }
 }
